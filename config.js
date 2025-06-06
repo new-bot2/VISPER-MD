6b2b5449be8065bcfc8369141194e51fe3b5e412
I-AM-CHAMI <88298450+chamiofficial@users.noreply.github.com>
2025-06-06 20:55:52 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'MOVIE-VISPER=zI1nWTTZ#0INKf4cvkxPtYgVUc-96bXmb0T2BpwAhp0rqWyONkL4-DILALK' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'MOVIE-VISPER=z441QTSA#Hxh8m2Nb42NTEkVYq2rmV7FMXGcG7ohHn0fCociQvWE' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
